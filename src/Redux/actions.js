fdf80e2ab1c5a85760da35a263b36ab34c4d89aa
manishfw13 <manish_fw13_347@masai.school>
2022-05-02 14:18:06 +0530

    first commit

---
+++ src/Redux/actions.js
@@ -0,0 +1,9 @@
+// action types
+export const LOGIN = "LOGIN";
+//export const MEET = "MEET"
+
+
+// Action Creators
+
+export const userLogin = () => ({ type: LOGIN, payload });
+//export const Meets = (data)=>({type:MEET, payload:data});
